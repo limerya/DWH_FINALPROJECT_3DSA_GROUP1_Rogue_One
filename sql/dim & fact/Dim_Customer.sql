157819cd8016c3f2a2ec1750798f27c93326ec40
2terrrr <justinebenedict.bautista.sci@ust.edu.ph>
2025-11-22 01:01:13 +0800

    Dim_category. Final

--- sql/dim & fact/Dim_Customer.sql
+++ sql/dim & fact/Dim_Customer.sql
@@ -1,17 +1,19 @@
+SELECT * FROM Dim_Customer;
+
 CREATE TABLE IF NOT EXISTS Dim_Customer (
-    Customer_Key SERIAL PRIMARY KEY,      -- Surrogate Key
-    user_id VARCHAR,                  -- Natural Key
+    Customer_Key SERIAL PRIMARY KEY,      
+    user_id VARCHAR,                 
     customer_name VARCHAR,
     job_title VARCHAR,
     job_level VARCHAR,
     credit_card_number VARCHAR,
     issuing_bank VARCHAR,
-    customer_gender VARCHAR,          -- Renamed
-    customer_street VARCHAR,         -- Renamed
-    customer_state VARCHAR,          -- Renamed
-    customer_city VARCHAR,           -- Renamed
-    customer_country VARCHAR,        -- Renamed
-    customer_user_type VARCHAR        -- Renamed
+    customer_gender VARCHAR,          
+    customer_street VARCHAR,         
+    customer_state VARCHAR,          
+    customer_city VARCHAR,           
+    customer_country VARCHAR,        
+    customer_user_type VARCHAR        
 );
 
 INSERT INTO Dim_Customer (
@@ -29,22 +31,20 @@ INSERT INTO Dim_Customer (
     customer_user_type
 )
 SELECT DISTINCT
-    sd.user_id,                -- From User Data
-    sd.name,                   -- From User Data
-    sj.job_title,              -- From Job Table
-    sj.job_level,              -- From Job Table
-    scc.credit_card_number,    -- From Credit Card Table
-    scc.issuing_bank,          -- From Credit Card Table
-    sd.gender,                 -- Renaming to Customer_Gender
-    sd.street,                 -- Renaming to Customer_Street
-    sd.state,                  -- Renaming to Customer_State
-    sd.city,                   -- Renaming to Customer_City
-    sd.country,                -- Renaming to Customer_Country
-    sd.user_type               -- Renaming to Customer_User_type
+    sd.user_id,               
+    sd.name,                  
+    sj.job_title,             
+    sj.job_level,              
+    scc.credit_card_number,    
+    scc.issuing_bank,          
+    sd.gender,                 
+    sd.street,                
+    sd.state,                  
+    sd.city,                   
+    sd.country,                
+    sd.user_type               
 FROM staging_user_data AS sd
 LEFT JOIN staging_user_job AS sj 
     ON sd.user_id = sj.user_id
 LEFT JOIN staging_user_credit_card AS scc 
     ON sd.user_id = scc.user_id;
-
-SELECT * FROM Dim_Customer;
