3674e59a7647d25dcfc953931ee452f63a2784cd
2terrrr <justinebenedict.bautista.sci@ust.edu.ph>
2025-11-22 00:54:22 +0800

    campaign

---
+++ sql/dim & fact/Dim_Campaign.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Dim_Campaign (
+    campaign_key SERIAL PRIMARY KEY,    
+    campaign_id VARCHAR,                
+    campaign_name VARCHAR,
+    discount DECIMAL (10,2)                       
+);
+
+INSERT INTO Dim_Campaign (campaign_id, campaign_name, discount)
+SELECT DISTINCT 
+    campaign_id, 
+    campaign_name, 
+    discount::DECIMAL (10,2)
+FROM staging_campaign_data;
+
+SELECT * FROM Dim_Campaign;
