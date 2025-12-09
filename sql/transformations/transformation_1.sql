d695f366dd9157ba2e844d3c20f587b155f727db
natetags <nathantagama@gmail.com>
2025-12-09 21:06:10 +0800

    Delete sql/transformations/transformation_1.sql

--- sql/transformations/transformation_1.sql
+++
@@ -1,6 +0,0 @@
-UPDATE staging_campaign_data
-SET discount = SUBSTRING(discount FROM '^[0-9]+');
-
-ALTER TABLE staging_campaign_data
-ALTER COLUMN discount TYPE NUMERIC(10,2) 
-USING discount::NUMERIC / 100.0;
